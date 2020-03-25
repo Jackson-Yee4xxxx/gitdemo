9b6895c86c39d9acf14a6878e2c55110d099fc08
Tan-Ya2000 <tanya@buaa.edu.cn>
2020-03-25 17:42:47 +0800

    update by two

--- fileinfo.py
+++ fileinfo.py
@@ -28,3 +28,41 @@ def change(coinValueList,change):
                 break                       #则跳出循环
             else:
                 change = newchange
+
+#2、定义函数，检查所求得的解是否为可行解                  
+def checkOk(change_res,oChange):               
+#change_res为存放结果的字典，键为某种硬币面值，对应的值为该种硬币的个数；oChange为需要找零的钱数
+    csum = 0                               # change_res中所有硬币钱数的和 
+    for key in change_res:                 #遍历字典change_res
+        csum = csum + key*change_res[key]  #将各种硬币面值与对应个数相乘，再相加
+    if csum == oChange:                    #若change_res中所有硬币钱数的和等于需要找零的钱数
+        return True
+    
+#3、定义函数，获得找零的硬币总个数
+def coinNum(change_res):                   #change_res为存放结果的字典             
+    num = 0                                #找零的硬币个数
+    for key in change_res:                 #遍历字典change_res
+        if change_res[key] != 0:
+            num = num + change_res[key]    #将所有的值相加，即得到找零的硬币总个数
+    return num
+
+#4、主函数
+if __name__ == '__main__':
+    clist = []                         #存放硬币种类列表
+    change_dict = {}                   #全局变量，存放结果的字典，键为某种硬币面值，对应的值为该种硬币的个数
+    #（1）输入找零数和硬币种类
+    cvalue = int(input('请输入找零数(单位：元)：'))
+    ctemp = int(input('请输入硬币种类，以非正数结束：'))
+    while ctemp > 0:                   #采用while语句，可以多次输入硬币种类
+        clist.append(ctemp)                #将输入的硬币种类添加到clist列表中
+        ctemp = int(input('请输入硬币种类，以非正数结束：'))        
+
+    #（2）调用change函数，采用贪心算法求得和为11的最优硬币组合，得到字典change_dict
+    change(clist,cvalue)                        
+
+   #（3）调用checkOk函数，检查所求得的解是否为可行解
+    if checkOk(change_dict, cvalue) == True:    
+        print ("找零结果：", change_dict)
+        print ("找零个数：", coinNum(change_dict))#（4）调用coinNum函数，获得找零的硬币个数
+    else:
+        print ("无解")
